b8d79d6c7e78acd22fb2e4b582c3810d891e47d1
Tobias Lindroth <31400061+rothen98@users.noreply.github.com>
2018-09-18 13:07:42 +0200

    Implemented a few methods

--- src/main/java/model/Channel.java
+++ src/main/java/model/Channel.java
@@ -1,36 +1,64 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 public class Channel implements IChannel {
+    private Collection<IUser> users;
+    private List<IMessage> messages;
+    private String name;
+
+    public Channel(String name) {
+        this.name = name;
+        this.users = new HashSet<>();
+        this.messages = new ArrayList<>();
+
+    }
+
+    /**
+     * This method will return  all the user names of this channel
+     * @return a collection with strings
+     */
     @Override
     public Collection<String> getAllUserNames() {
-        return null;
+        Collection<String> returnCollection = new ArrayList<>();
+        users.forEach(s ->{
+            returnCollection.add(s.getName());
+        });
+
+        return returnCollection;
     }
 
     @Override
     public List<IMessage> getAllMessages() {
-        return null;
+        return copyOfList(messages);
+    }
+
+    private List<IMessage> copyOfList(List<IMessage> messages) {
+        return messages;
+        //TODO make this method
     }
 
     @Override
-    public List<IMessage> getLastMessages(int index) {
-        return null;
+    public List<IMessage> getLastMessages(int amount) {
+        return messages.subList(messages.size()-amount,messages.size());
+        //Todo does this really return a copy?
     }
 
     @Override
     public void join(IUser user) {
-
+        users.add(user);
     }
 
     @Override
     public void leave(IUser user) {
-
+        users.remove(user);
     }
 
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 }
--- src/main/java/model/IUser.java
+++ src/main/java/model/IUser.java
@@ -2,4 +2,5 @@ package model;
 
 public interface IUser {
     void sendMessageToClients(IMessage message);
+    String getName();
 }
