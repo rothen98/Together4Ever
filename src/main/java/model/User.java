f376c7952de3e6285c0e6da0a2b0e789cdf35c38
Viktor Franzén <viktor_franzen@hotmail.com>
2018-09-19 18:56:03 +0200

    Added some javadoc to a method. Created a method that can connect clients to the user.

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -7,7 +7,7 @@ import java.util.Collection;
 /**
  * @author Viktor Franzén
  *
- * This class contains the data of the User. Mainly an identifcation name and all it´s clients.
+ * This class contains the data of the User. Mainly an identifcation name, password and all it´s clients.
  */
 public class User implements IUser {
 
@@ -22,14 +22,28 @@ public class User implements IUser {
     }
 
 
+
     /**
-     * Sends a message to all the contained clients.
+     * Checks if the password is identical to the objects password.
+     * If true it adds it to the clients list.
+     * @param client The client that wants to be connected
+     * @param password The password that needs to match the objects password.
+     */
+    public void connectClient(IClient client, String password){
+        if(this.password.equals(password)){
+            clients.add(client);
+        }
+    }
+
+    /**
+     * Sends a message to all the connected clients in the objects collection.
      * @param message The message that should be sent to the connected clients.
      */
     @Override
     public void sendMessageToClients(IMessage message) {
         clients.forEach(x -> x.update(message));
     }
+
 
     @Override
     public String getName() {
