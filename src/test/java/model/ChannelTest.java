3d9f4aa1dcc99d44983b3cc67c81e82c942dc31a
Tobias Lindroth <31400061+rothen98@users.noreply.github.com>
2018-09-19 17:24:29 +0200

    Added comments on how tests are to be implemented

--- src/test/java/model/ChannelTest.java
+++ src/test/java/model/ChannelTest.java
@@ -19,14 +19,20 @@ public class ChannelTest {
     @Test
     public void getAllUserNames() {
         Channel channel = new Channel("Together4Ever");
+        //Create two users, let them join, and get all users names
     }
 
     @Test
     public void getAllMessages() {
+        Channel channel = new Channel("Together4Ever");
+        //Create three messages, send them, and check if the channel's messages contains the same
+        //info as the three created messages
     }
 
     @Test
     public void getLastMessages() {
+        //Create three messages, send team, and check if the channel's two latest messages contains the same
+        // info as the two messages you sent last.
     }
 
     @Test
@@ -39,5 +45,6 @@ public class ChannelTest {
 
     @Test
     public void getName() {
+        //Create a channel and check if the method returns the gicen name
     }
 }
