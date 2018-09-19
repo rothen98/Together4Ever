673d9f4a8e6068a161eb713b63fb39ca5fa3fd49
Viktor Franzén <viktor_franzen@hotmail.com>
2018-09-19 19:05:28 +0200

    Added a test class with matching testmethods for the user class. No tests written yet.

---
+++ src/test/java/model/UserTest.java
@@ -0,0 +1,24 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void connectClient() {
+    }
+
+    @Test
+    public void removeClient() {
+    }
+
+    @Test
+    public void sendMessageToClients() {
+    }
+
+    @Test
+    public void getName() {
+    }
+}
