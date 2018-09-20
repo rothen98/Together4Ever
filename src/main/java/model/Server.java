807891cb9e53709090543579c8acfa29900c279d
Tobias Lindroth <31400061+rothen98@users.noreply.github.com>
2018-09-20 17:07:09 +0200

    Created most of the server methods

---
+++ src/main/java/model/Server.java
@@ -0,0 +1,89 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+public class Server implements IServer {
+    private Collection<IUser> users;
+    private Collection<IChannel> channels;
+
+    public Server() {
+        this.users = new HashSet<>();
+        this.channels = new HashSet<>();
+    }
+
+    /**
+     * This method returns all the channels the given user is a member of.
+     * It asks each existing method if it has the given user, and if it does, the channel are
+     * added to the collection that are to be returned.
+     * @param user This is the user whose channels you want to receive
+     * @return A collection with the channels the given user is a member of
+     */
+    @Override
+    public Collection<IChannel> getUsersChannels(IUser user) {
+        final Collection<IChannel> channelsToReturn = new ArrayList<>();
+        channels.forEach(channel -> {
+            //TODO
+            /*if(channel.hasUser(user)){ //We need to be able to check if a channel has a user
+                channelsToReturn.add(channel);
+            }*/
+        });
+        return channelsToReturn;
+    }
+
+    /**
+     * This method will return all the channels names.
+     * @return A collection with the channels names
+     */
+    @Override
+    public Collection<String> getChannelNames() {
+        final Collection<String> channelNames = new ArrayList<>();
+        channels.forEach(channel -> {
+            channelNames.add(channel.getName());
+        });
+        return channelNames;
+    }
+
+    /**
+     * This method will check if their is an channel with the given id.
+     * If it does, this channel will be returned.
+     * @param id
+     * @return the channel with the given id
+     */
+    @Override
+    public IChannel getChannel(int id) {
+        channels.forEach(channel -> {
+            //Todo
+            /*if(channel.getID() == id){
+                return true;
+            }*/
+        });
+        //throw NoChannelFoundException if no channel found
+        return null;
+    }
+
+
+    @Override
+    public IUser getUser(String name, String password) {
+        users.forEach(user->{
+            /*if(user.authorize(name,password)){
+                return user;
+            }*/
+        });
+        //throw NoUserFoundException if user not found
+        return null;
+
+    }
+
+    @Override
+    public void addChannel(IChannel channel) {
+        channels.add(channel);
+    }
+
+    @Override
+    public void addUser(IUser user) {
+        users.add(user);
+    }
+}
