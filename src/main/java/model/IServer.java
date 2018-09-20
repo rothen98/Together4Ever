a88bb10b93126dd855e345e7e2f036ec13264fdf
Tobias Lindroth <31400061+rothen98@users.noreply.github.com>
2018-09-20 17:07:09 +0200

    Created most of the server methods

--- src/main/java/model/IServer.java
+++ src/main/java/model/IServer.java
@@ -6,13 +6,12 @@ import java.util.Collection;
 public interface IServer {
     Collection<IChannel> getUsersChannels(IUser user);
     Collection<String> getChannelNames();
-    IChannel getChannel(String name);
-    IChannel createNewChannel(String name);
+
+    IChannel getChannel(int id);
     IUser getUser(String name, String password);
+
+    void addChannel(IChannel channel);
     void addUser(IUser user);
-
-
-
 
 
 }
