035a819126db04ee6937bde3cac21813ab16736a
Solen <alex.solberg@hotmail.com>
2018-09-20 09:53:45 +0200

    create test class

--- src/test/java/model/MessageTest.java
+++ src/test/java/model/MessageTest.java
@@ -1,5 +1,30 @@
+package model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MessageTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getMessage() {
+    }
+
+    @Test
+    public void getTimestamp() {
+    }
+
+    @Test
+    public void getSender() {
+    }
 }
