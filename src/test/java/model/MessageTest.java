c693a0f81c9a11cd9590ab846db98d1fcaa29d19
Solen <alex.solberg@hotmail.com>
2018-09-23 18:15:51 +0200

    Created a few simple tests for message class

--- src/test/java/model/MessageTest.java
+++ src/test/java/model/MessageTest.java
@@ -4,27 +4,57 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MessageTest {
 
+    private Message message;
+    private String textMessage = "Testing Message";
+    private IUser sender;
+    private LocalDateTime timestamp = LocalDateTime.now();
+
     @Before
     public void setUp() throws Exception {
+        message = new Message(sender, textMessage, timestamp);
     }
 
     @After
     public void tearDown() throws Exception {
+        message = null;
     }
 
     @Test
     public void getMessage() {
+        //just creating a message and checking that it cant be changed
+        String newMessage = message.getMessage();
+        String testMessage = "These wont be the same";
+        //newMessage = testMessage;
+        assertNotEquals(newMessage, testMessage);
+        assertTrue(newMessage.length() == 15);
+        //Will probably test something more here.
     }
 
     @Test
     public void getTimestamp() {
+        int day = timestamp.getDayOfMonth();
+        int month = timestamp.getMonthValue();
+        int year = timestamp.getYear();
+        int hour = timestamp.getHour();
+        int minute = timestamp.getMinute();
+        int second = timestamp.getSecond();
+
+        assertTrue(day == 23);
+        //Ill do the same for the other variables
+
     }
 
     @Test
     public void getSender() {
+        sender = new User("Name", "Password");
+        //Dont know what Ill do for test right now...
     }
 }
