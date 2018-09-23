200a1ffd4eff665da4a79662a18fae241c6a8610
Solen <alex.solberg@hotmail.com>
2018-09-23 18:20:25 +0200

    Updated spelling and message test

--- src/test/java/model/MessageTest.java
+++ src/test/java/model/MessageTest.java
@@ -15,6 +15,7 @@ public class MessageTest {
     private Message message;
     private String textMessage = "Testing Message";
     private IUser sender;
+    private IUser sender2;
     private LocalDateTime timestamp = LocalDateTime.now();
 
     @Before
@@ -54,7 +55,10 @@ public class MessageTest {
 
     @Test
     public void getSender() {
-        sender = new User("Name", "Password");
+        sender = new User("Name", "Password12");
+        sender2 = new User("TestName", "Password23");
+
+        assertNotEquals(sender, sender2);
         //Dont know what Ill do for test right now...
     }
 }
