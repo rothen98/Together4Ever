aa3aefdd6d11acdf960fa1b5abda13a65ebf6509
Viktor Franzén <viktor_franzen@hotmail.com>
2018-09-25 11:57:59 +0200

    Finished all the tests for the User class.

--- src/main/java/model/IUser.java
+++ src/main/java/model/IUser.java
@@ -1,6 +1,10 @@
 package model;
 
 public interface IUser {
+    void connectClient(IClient client, String password);
+    void removeClient(IClient client, String password);
     void sendMessageToClients(IMessage message);
     String getName();
+    int getAmountOfClients();
+
 }
