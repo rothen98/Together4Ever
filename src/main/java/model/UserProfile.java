2f5e346d2eb6d4c88e86d49973d8edab556145fc
Viktor Franzén <viktor_franzen@hotmail.com>
2018-10-01 14:32:24 +0200

    Begun implementing the userProfile. Done the most basic methods. Not anything about changing displayName and so on yet.

---
+++ src/main/java/model/UserProfile.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.awt.*;
+
+/**
+ * @author Viktor Franzén
+ * The UserProfile class is supposed to contain the information that are going to be displayed
+ * to other users in the application and should be easy to change for the user.
+ */
+public class UserProfile implements IIdentifiable {
+
+    private String displayName;
+    private Image displayImage;
+
+    public UserProfile(String displayName){
+        this.displayName = displayName;
+        //Set displayImage to the path to an default displayImage
+    }
+
+
+
+
+
+
+    /**
+     * Returns the objects displayName.
+     * @return String with the displayName.
+     */
+    @Override
+    public String getDisplayName(){
+        return this.displayName;
+    }
+
+    /**
+     * Returns the objects displayImage.
+     * @return The displayImage.
+     */
+    @Override
+    public Image getDisplayImage() {
+        return null;
+    }
+}
