03788c023eec511f9c37e7f4486dd2f399b57d5c
Viktor Franzén <viktor_franzen@hotmail.com>
2018-10-08 11:28:05 +0200

    Created the test class. No test implemented yet.

---
+++ src/test/java/model/MessageFactoryTest.java
@@ -0,0 +1,35 @@
+package model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MessageFactoryTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void createTextMessage() {
+        
+    }
+
+
+    @Test
+    public void createImageMessage() {
+
+    }
+
+
+}
