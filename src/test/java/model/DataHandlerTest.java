448542d9f58470a599ce08f1675a968d3c19300f
Solen <alex.solberg@hotmail.com>
2018-10-10 22:43:10 +0200

    created test class

---
+++ src/test/java/model/DataHandlerTest.java
@@ -0,0 +1,47 @@
+package model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileWriter;
+
+import static org.junit.Assert.*;
+
+public class DataHandlerTest {
+
+    private DataHandler dataHandler = new DataHandler();
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void loopUsers() {
+    }
+
+    @Test
+    public void loopChannels() {
+    }
+
+    @Test
+    public void pushUser() {
+    }
+
+    @Test
+    public void pushChannel() {
+    }
+
+    @Test
+    public void getUsers() {
+    }
+
+    @Test
+    public void getChannels() {
+    }
+}
