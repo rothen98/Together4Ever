2d5f3f058c81f14b82d805cd5ad52870bb867d39
Viktor Franzén <viktor_franzen@hotmail.com>
2018-10-15 12:07:08 +0200

    Added tests to the TextContent class.

---
+++ src/test/java/model/chatcomponents/message/TextContentTest.java
@@ -0,0 +1,33 @@
+package model.chatcomponents.message;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import sun.plugin2.message.Message;
+
+import static org.junit.Assert.*;
+
+public class TextContentTest {
+
+    private IMessageContent content;
+
+    @Before
+    public void setUp() throws Exception {
+        content = new TextContent("test");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        content = null;
+    }
+
+    @Test
+    public void getType() {
+        assertEquals(MessageType.TEXT, content.getType());
+    }
+
+    @Test
+    public void getMessage() {
+        assertEquals("test", content.getMessage());
+    }
+}
