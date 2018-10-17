1ca18c6db8fd385dc869fa51f00256421d8a3d93
Solen <alex.solberg@hotmail.com>
2018-10-17 10:45:23 +0200

    Wrote Jdoc for the user methods

--- src/main/java/services/datahandler/DataHandler.java
+++ src/main/java/services/datahandler/DataHandler.java
@@ -14,6 +14,11 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
+/**
+ * This class is responsible for collecting all the data that exists on the program and saving it to a file.
+ * The class also collects the data from the file and sends it back to the server when the program starts up.
+ * @author Alex Solberg
+ */
 public class DataHandler implements IDataHandler {
 
     private FileWriter channelFile;
@@ -41,6 +46,12 @@ public class DataHandler implements IDataHandler {
 
     }
 
+    /**
+     * This method creates a JSONArray by inserting the information that it gets from a specific JSON file
+     *
+     * @param filename is the file which has the saved data
+     * @return an array with all the information that was saved on the file
+     */
     private JSONArray createJSONArrayFromFile(String filename){
         try {
             String data = readFile(filename);
@@ -55,6 +66,13 @@ public class DataHandler implements IDataHandler {
 
     }
 
+    /**
+     * This method builds a string with the data from all of the information from the JSON file
+     *
+     * @param filename is the filepath which contains the saved data
+     * @return a string with the information from the JSON file
+     * @throws IOException
+     */
     private String readFile(String filename) throws IOException {
         String result = "";
 
@@ -70,17 +88,35 @@ public class DataHandler implements IDataHandler {
         return result;
     }
 
+    /**
+     * This method is responsible for looping through the array and transferring contents into a JSON object
+     *
+     * @param users is a collection of data about users that holds all the information about them
+     */
     private void loopUsers(Collection<UserData> users) {
         for (UserData u : users) {
             writeUserArray.put(createNewUserJObject(u));
             }
     }
 
+    /**
+     * This method creates a new JSONObject with all the user data
+     *
+     * @param u is a user object that contains all of a users data
+     * @return a JSON object with the users data
+     */
     private JSONObject createNewUserJObject(UserData u) {
         JSONObject user = new JSONObject();
         initUserJObject(u,user);
         return user;
     }
+
+    /**
+     * This method initializes a JSON object by inserting the values by getting their respective keys
+     *
+     * @param u is an object that contains all the users data
+     * @param userJ is a translatable user object that can be used to get key values
+     */
     private void initUserJObject(UserData u,JSONObject userJ){
         userJ.put("Username", u.getUsername());
         userJ.put("Password", u.getPassword());
@@ -89,6 +125,11 @@ public class DataHandler implements IDataHandler {
     }
 
 
+    /**
+     * 
+     *
+     * @param channels
+     */
     private void loopChannels(Collection<ChannelData> channels) {
         for (ChannelData c : channels) {
                 JSONObject channel = createNewChannelJObject(c);
