8bd80c8724811434a76b1991166b41f15ebeb2d1
Viktor Franzén <viktor_franzen@hotmail.com>
2018-10-18 12:01:16 +0200

    Created interface and adapter for the password encryption library.

---
+++ src/main/java/services/PasswordEncryption/PasswordEncryption.java
@@ -0,0 +1,12 @@
+package services.PasswordEncryption;
+
+/**
+ * Interface for encrpyting passwords in the application. Removes the direct dependency on
+ * the encryption library from the model.
+ */
+public interface PasswordEncryption {
+    String hashPassword(String password, String salt);
+    boolean checkPassword(String password, String hashedPassword);
+    String generateSalt();
+
+}
